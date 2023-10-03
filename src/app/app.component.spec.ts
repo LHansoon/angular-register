2b4a2c93329a984c77ec4fe3f2bd09992bd0a35d
hanson <hn427433@dal.ca>
2023-10-03 16:31:59 -0300

    update unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
         const fixture = TestBed.createComponent(AppComponent);
         fixture.detectChanges();
         const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('h1')?.textContent).not.toContain('registration app is running!');
+        expect(compiled.querySelector('h1')?.textContent).not.toContain('Profile');
     });
 });
